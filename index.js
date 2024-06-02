6f6580eb51b7fff34b923c707f68d152041b70e2
Shahadath Hossen Sajib <shahadathhossensajib732@gmail.com>
2024-06-02 06:37:10 +0600

    on register user data stored in database

--- index.js
+++ index.js
@@ -83,16 +83,16 @@ async function run() {
     // }
 
     // user related api
-    // app.post('/users', async (req, res) => {
-    //   const user = req.body;
-    //   const query = { email: user.email }
-    //   const existingUser = await usersCollection.findOne(query);
-    //   if (existingUser) {
-    //     return res.send({ message: 'user already exists', insertedId: null })
-    //   }
-    //   const result = await usersCollection.insertOne(user);
-    //   res.send(result);
-    // });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
     // location related api
     app.get("/location", async(req, res) => {
